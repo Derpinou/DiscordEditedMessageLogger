33127a7dcb4abc43cfa6778439146d7a458e6a4b
Derpinou <lightcompte@gmail.com>
2020-11-26 21:02:50 +0100

    update

---
+++ config.sample.js
@@ -0,0 +1,8 @@
+module.exports = {
+    bot: {
+        token: ""
+    },
+    log: {
+        channelID: ""
+    }
+}
---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client, MessageEmbed } = require('discord.js'),
+    client = new Client(),
+    config = require('./config');
+
+client
+    .on("ready", () => {
+        console.log("Je suis prêt");
+    })
+    .on("messageUpdate", (oldMessage, newMessage) => {
+        if (oldMessage.content !== newMessage.content) {
+            let channel = newMessage.channel.guild.channels.cache.get(config.log.channelID)
+            if (channel) {
+                let embed = new MessageEmbed()
+                    .addField("Auteur:", newMessage.author.tag)
+                    .addField("Ancien message:",  oldMessage.content)
+                    .addField("Nouveau message:",  newMessage.content)
+                    .setTimestamp();
+                return channel.send(embed)
+            }
+        }
+    })
+    .login(config.bot.token)
